b1b44233a2d269853cd81c378809b0b29e866df1
jc <chan_j@naver.com>
2018-05-03 13:18:54 +0900

    add product DAO / add product Command

--- src/com/dankook/EGINE_MANAGE/Command/ProductCommand/ProductSearchCommand.java
+++ src/com/dankook/EGINE_MANAGE/Command/ProductCommand/ProductSearchCommand.java
@@ -1,14 +1,38 @@
 package com.dankook.EGINE_MANAGE.Command.ProductCommand;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dankook.EGINE_MANAGE.Dao.ProductDao;
+import com.dankook.EGINE_MANAGE.Dto.ProductDto;
 
 public class ProductSearchCommand implements ProductCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-
+		
+		// UTF-8 인코딩
+		try {
+			request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		// 물품 이름을 가져옴
+		String productName = request.getParameter("productName");
+		
+		// 물품 DAO 객체 생성 후 물품 검색 로직 실행
+		ProductDao dao = new ProductDao();
+		ArrayList<ProductDto> dtos = dao.searchProduct(productName);
+		
+		// 물품 검색 성공
+		if(dtos != null) {
+			HttpSession httpSession = request.getSession();
+			httpSession.setAttribute("ProductSearch", dtos);
+		}
 	}
-
 }
--- src/com/dankook/EGINE_MANAGE/Dao/ProductDao.java
+++ src/com/dankook/EGINE_MANAGE/Dao/ProductDao.java
@@ -2,10 +2,14 @@ package com.dankook.EGINE_MANAGE.Dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.dankook.EGINE_MANAGE.Dto.ProductDto;
 
 public class ProductDao {
 	// DaataSource 가져오는 과정
@@ -21,7 +25,6 @@ public class ProductDao {
 			e.printStackTrace();
 		}
 	}
-	
 	
 	// 물품 추가 로직
 	// 반환: 성공값 / 매개변수: 물품 이름
@@ -72,6 +75,158 @@ public class ProductDao {
 	
 	// 물품 삭제 로직
 	public int deleteProduct(int productNumber) {
+		// 쿼리문, 연결 객체
+		PreparedStatement preStatement = null;
+		Connection conn = null;
+		String query = null;
 		
+		// 성공값 1:성공, 0: 실패
+		int run = 0;
+		
+		try {
+			// 커넥션 객체 가져오기
+			conn = dataSource.getConnection();
+			
+			// 쿼리문
+			query =  "delete from product where ProductNumber = ?";
+			
+			// prepared Statement에 쿼리문 넣기
+			preStatement = conn.prepareStatement(query);
+			
+			// prepared Statement 쿼리문 만들기
+			preStatement.setInt(1, productNumber);
+			
+			// 쿼리문 실행
+			run = preStatement.executeUpdate();
+			System.out.println("Product Delete 성공");
+			
+		} catch (Exception e) {
+			// 쿼리 에러
+			e.printStackTrace();
+			System.out.println("Product Delete 실패");
+			
+		} finally {
+			// 커넥션 객체 닫기
+			try {
+				if (preStatement != null) { preStatement.close(); }
+				if (conn != null) { conn.close(); }
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return run;
+	}
+	
+	// 물품 검색 로직
+	public ArrayList<ProductDto> searchProduct(String productName) {
+		
+		// 쿼리문, 연결 객체
+		PreparedStatement preStatement = null;
+		Connection conn = null;
+		ResultSet resultSet = null;
+		String query = null;
+		
+		// 물품 DTO객체, 물품 리스트
+		ProductDto product = null;
+		ArrayList<ProductDto> dtos = new ArrayList<ProductDto>();
+		
+		// 물품 검색 로직 시작
+		try {
+			// 커넥션 객체 얻기
+			conn = dataSource.getConnection();
+			
+			// 쿼리문 생성
+			query = "select * from product where ProductName like %?%";
+			
+			// 쿼리문 완성
+			preStatement = conn.prepareStatement(query);
+			preStatement.setString(1, productName);
+			
+			// 쿼리문 실행 후 결과 얻기
+			resultSet = preStatement.executeQuery(query);
+			
+			// 물품 객체 받아오기
+			while(resultSet.next()) {
+				
+				// 객체 정보 받아오기
+				int productNumber = resultSet.getInt("ProductNumber");
+				String productName1 = resultSet.getString("ProductName");
+				
+				// 객체 생성 후 리스트에 넣기
+				product = new ProductDto(productNumber, productName1);
+				dtos.add(product);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				
+				// 사용한 객체 반납
+				if (preStatement != null) { preStatement.close(); }
+				if (conn != null) { conn.close(); }
+				if (resultSet != null) { resultSet.close(); }
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return dtos;
+	}
+	
+	// 물품 리스트 로직
+	public ArrayList<ProductDto> listProduct() {
+		
+		// 쿼리문, 연결 객체
+		PreparedStatement preStatement = null;
+		Connection conn = null;
+		ResultSet resultSet = null;
+		String query = null;
+		
+		// 물품 DTO객체, 물품 리스트
+		ProductDto product = null;
+		ArrayList<ProductDto> dtos = new ArrayList<ProductDto>();
+		
+		// 물품 리스트 로직 시작
+		try {
+			// 커넥션 객체 얻기
+			conn = dataSource.getConnection();
+			
+			// 쿼리문 생성
+			query = "select * from product";
+			
+			// 쿼리문 완성
+			preStatement = conn.prepareStatement(query);
+			
+			// 쿼리문 실행 후 결과 얻기
+			resultSet = preStatement.executeQuery(query);
+			
+			// 물품 객체 받아오기
+			while(resultSet.next()) {
+				
+				// 객체 정보 받아오기
+				int productNumber = resultSet.getInt("ProductNumber");
+				String productName1 = resultSet.getString("ProductName");
+				
+				// 객체 생성 후 리스트에 넣기
+				product = new ProductDto(productNumber, productName1);
+				dtos.add(product);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				// 사용한 객체 반납
+				if (preStatement != null) { preStatement.close(); }
+				if (conn != null) { conn.close(); }
+				if (resultSet != null) { resultSet.close(); }
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return null;
 	}
 }
